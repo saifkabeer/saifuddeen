87f9d237240062f7ef4cfd9476063fed118a37bb
saifkabeer <70325102+saifkabeer@users.noreply.github.com>
2021-08-05 11:25:14 +0100

    Add files via upload

---
+++ ICTHUB WEB D/app.js
@@ -0,0 +1,14 @@
+///let element;
+//elememt = document.head
+//elememt = document.body
+//elememt = document.domain
+//console.log(element);
+<script>
+function changeitem(){
+    document.querySelector('h1')
+     .innerHTML ='we Create innovate and Explore'
+}
+  </script>
+  let btn = document.querySelector
+  ('button')
+  btn.addEventListener('click', changeItem)
